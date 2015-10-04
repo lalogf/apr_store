f0d752d8a8228fc911cca532bb22e364e6ee14d1
lalogf <lalogf@gmail.com>
2015-10-04 01:35:30 -0500

    More front end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,19 +23,49 @@ var ready = function (){
 	// if ($("form#new_collection .form-group").attr("class") == "form-group has-error has-feedback"){
 	// 	$("#crearColeccion").modal("show");
 	// }
+	$(".navbar").hide();
 	if ((($("form#new_collection .has-error")).length > 0)) {
 		$("#crearColeccion").modal("show");
 		console.log("hello error coleccion");
 	}
 	else if (($("form#new_designer .has-error").length) > 0) {
 		$("#crearDisenador").modal("show");
+	};
+	var scroll_start = 0;
+	var startchange = $('#startchange');
+	var offset = startchange.offset();
+	if (startchange.length){
+		$(document).scroll(function() { 
+			scroll_start = $(this).scrollTop();
+			// if(scroll_start > offset.top) {
+			// 	$(".navbar-default").css('background-color', 'rgba(255,255,255,0.8)');
+			// } else {
+			// 	$('.navbar-default').css('background-color', 'transparent');
+			// }
+			if(scroll_start > offset.top) {
+				$(".navbar-default").show().css('background-color','rgba(255,255,255,1)');
+			} else {
+				$('.navbar-default').hide();
+			}
+		});
 	}
+	// $(function(){
+	// 	$("#pins").masonry({
+	// 		itemSelector: '.box',
+	// 		isFitWidth: true
+	// 	});
 
-	// $(".title").each(function(){
-	// 	if(($(this).html().length)>30){
-	// 		$(this).attr("style","font-size:18px;")
-	// 	}
 	// });
+$(".banner").hover(function(){
+	$(this).addClass('bannerHover');
+	$(".marca", this).css('color','white');
+	$(".ver").toggle();
+}, function(){
+	$(this).removeClass('bannerHover');
+	$(".marca",this).css('color','black');
+	$(".ver").toggle();
+});
+
 
 };
 
