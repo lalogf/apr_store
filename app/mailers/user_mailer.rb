37c12d1cc29833ccc68e535f995c1186725bffe6
lalogf <lalogf@gmail.com>
2016-01-24 02:35:25 -0500

    fixes in emailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: 'Lalo de ArteAparte <lalo@arteaparte.pe>'
  
-  def purchase_confirmation(user)
+  def purchase_confirmation(user, product)
     @user = user
+    @product = product
     mail(to: @user.email, subject: 'Gracias por comprar en ArteAparte')
   end
 end
